6af70daa74c4c76634c3b7c9b240899d89dede2c
Berkay Sargın <berkaey@gmail.com>
2018-03-23 21:11:47 +0300

    demo init.

---
+++ .scenes/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Scenes } from "react-scenes";
+
+import buttonScene from "./scenes/button";
+
+const initialContent = `<!DOCTYPE html>
+<html>
+<head>
+  <style>
+
+    *,
+    *:before,
+    *:after {
+      box-sizing: inherit;
+    }
+
+    *:focus {
+      outline: none;
+    }
+
+    html {
+      box-sizing: border-box;
+    }
+    
+    body {
+      margin:0;
+      padding:0;
+    }
+
+  </style>
+</head>
+<body>
+  <div id="frame"></div>
+</body>
+</html>`;
+
+let frame;
+
+frame = {
+  initialContent,
+  mountTarget: "#frame"
+};
+
+let panels = [];
+let actions = [];
+
+let config = {
+  panel: {
+    position: "right" // left, right, top, bottom
+  }
+};
+
+class Library extends Component {
+  render() {
+    return (
+      <Scenes
+        title="🌄 My Scenes"
+        config={config}
+        scenes={[buttonScene]}
+        frame={frame}
+        actions={actions}
+        panels={panels}
+      />
+    );
+  }
+}
+
+export default Library;
---
+++ .scenes/src/scenes/button.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { controllers } from "react-scenes";
+
+import Button from "../components/button";
+
+const docs = `## Button 
+  I'm a button.
+`;
+
+export default {
+  title: "Buttons>Default",
+  target: Button,
+  controllers: [
+    {
+      key: "title",
+      title: "Title",
+      controller: controllers.text("Button")
+    },
+    {
+      key: "theme",
+      title: "Theme",
+      controller: controllers.select("primary", [
+        { key: "Primary", value: "primary" },
+        { key: "Positive", value: "positive" },
+        { key: "Destructive", value: "destructive" }
+      ])
+    },
+    {
+      key: "big",
+      title: "Big?",
+      controller: controllers.boolean(false)
+    },
+    {
+      key: "small",
+      title: "Small?",
+      controller: controllers.boolean(false)
+    }
+  ],
+  events: ["onClick"],
+  options: {
+    theme: "light", // ligth, dark, white, black
+    centered: true
+  },
+  docs
+};
